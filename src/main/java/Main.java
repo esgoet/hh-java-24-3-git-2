87c12eb1914ac9b96f9622b1112f6bead32abee0
esgoet <toddesgo@gmail.com>
2024-07-12 15:54:07 +0200

    Add another student

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student("Eva", 27, "1230");
         System.out.println(student);
+        Student student2 = new Student("Chiara", 29, "1456");
+        System.out.println(student2);
     }
 }
